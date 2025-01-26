d6c852a5a9025d179abedcf2e999e37e3b613fac
Scott Bukowski <scottbukowski@hotmail.com>
2025-01-26 15:47:19 +0000

    Structure redesign.

---
+++ js/owzat-game.js
@@ -0,0 +1,101 @@
+const uiMessageBox = OwzatUI_CreateMessageBox();
+
+const cmdBowlBall = OwzatUI_CreateButton({ caption: "Bowl Ball", func: bowlBall });
+const cmdBowlOver = OwzatUI_CreateButton({ caption: "Bowl Over", func: bowlOver });
+const cmdViewScorecard = OwzatUI_CreateButton({ caption: "View Scorcard", func: viewScorecard });
+const cmdBar = document.createElement("div");
+
+const ozInnings = new OwzatInningsRecord(teams[0], teams[1]);
+
+function initialise() { //=====================================================
+    // set document styles
+    document.body.style.fontFamily = "Calibri";
+    document.body.style.fontSize = "10pt";
+
+    // create command bar
+    cmdBar.appendChild(cmdBowlBall);
+    cmdBar.appendChild(cmdBowlOver);
+    cmdBar.appendChild(cmdViewScorecard);
+    document.body.appendChild(cmdBar);
+
+    // create ui message box
+    document.body.appendChild(uiMessageBox);
+
+    let msg = document.createElement("div");
+    msg.style.padding = "1px 5px";
+    msg.style.borderBottomWidth = "1px";
+    msg.style.borderBottomStyle = "solid";
+    msg.style.borderBottomColor = "rgba(0,0,0,1)";
+    msg.style.backgroundColor = "rgba(0,0,0,0.2)";
+    msg.innerHTML = "<b>" + ozInnings.teamNameAtBat + "</b> bat first.";
+    uiMessageBox.insertAdjacentElement("afterbegin", msg);
+}
+
+function bowlBall() { //=======================================================
+    // create message
+    let htmMessage = "<i>" + ozInnings.overCount + "." + ozInnings.overBall + "</i> : ";
+    htmMessage += "<b>" + ozInnings.playerNameAtBowl + "</b> bowls to ";
+    htmMessage += "<b>" + ozInnings.playerNameAtBat + "</b>... ";
+
+    // create message container
+    let msg = document.createElement("div");
+    msg.style.padding = "1px 5px";
+    msg.style.borderBottomWidth = "1px";
+    msg.style.borderBottomStyle = (ozInnings.overBall == 1) ? "solid" : "dotted";
+    msg.style.borderBottomColor = (ozInnings.overBall == 1) ? "rgba(0,0,0,1)" : "rgba(0,0,0,0.2)";
+
+    let result = ozInnings.ProcessBowl() // BOWL THE BALL
+
+    htmMessage += (result.runs == 0) ? "no runs were scored." : "and scores <b>" + result.runs + "</b>.";
+
+    // set message in container
+    msg.innerHTML = htmMessage;
+    // add message to message box
+    uiMessageBox.insertAdjacentElement("afterbegin", msg);
+}
+
+function bowlOver() { //=======================================================
+    // const ballsToCompleteOver = ozInnings.ballsLeftInOver;
+    // for (let counter = 0; counter < ballsToCompleteOver; counter++) {
+    //     console.log(counter, ozInnings.ballsLeftInOver);
+    //     bowlBall()
+    // }
+    console.log("Bowl Over to be implemented.");
+}
+
+function viewScorecard() { //==================================================
+    console.log("View Scorecard to be implemented.");
+}
+
+// HTML Element creation functions ////////////////////////////////////////////
+
+function OwzatUI_CreateButton(params) { //=====================================
+    // create button element
+    let btn = document.createElement("button");
+
+    // set default button style
+    btn.type = "button";
+    btn.textContent = params.caption;
+    btn.style.font = "inherit";
+    btn.style.margin = "2px";
+
+    // set event listener for click event
+    btn.addEventListener("click", (ev) => {
+        params.func();
+    })
+
+    return btn;
+}
+
+function OwzatUI_CreateMessageBox() { //=======================================
+    // create div element
+    let box = document.createElement("div");
+
+    // set required div style
+    box.style.margin = "5px 0";
+    box.style.overflowY = 'auto';
+    box.style.borderTop = "solid 1px black";
+    box.style.height = '500px';
+
+    return box;
+}
--- js/owzat.js
+++ js/owzat.js
@@ -1,122 +1,174 @@
-const boxMessage = BfxCreateMessageBox();
 
-var ballCounter = 0;
-const inningsRecord = [new TeamInningsRecord(), new TeamInningsRecord()];
-var idBattingTeam = BfxRandomInt(2);
-var idBowlingTeam = Math.abs(idBattingTeam - 1);
-const bowlingPair = [0, 0];
-var idBowler = BfxRandomInt(2);
-const battingPair = [0, 0];
-var idBatsman = BfxRandomInt(2);
+class OwzatInningsRecord { //==================================================
+    #teamBat = null;
+    #teamBatName = '';
+    #teamBowl = null;
+    #teamBowlName = '';
 
-function initialise() {
-    // to do
-    document.body.appendChild(BfxCreateButton({
-        caption: "Bowl Ball",
-        func: BfxBowlBall
-    }));
-    document.body.appendChild(BfxCreateButton({
-        caption: "Bowl Over",
-        func: BfxBowlOver
-    }));
-    document.body.appendChild(BfxCreateButton({
-        caption: "View Scorcard",
-        func: () => { console.log("Scorecard to be implemented."); }
-    }));
+    #teamBatOrder = null;
+    #currentBattingPair = [0, 1];
+    #currentBatter = 0;
+    #teamBowlOrder = null;
+    #currentBowlingPair = [0, 1];
+    #currentBowler = 0;
 
-    document.body.style.fontFamily = "Calibri";
-    document.body.style.fontSize = "10pt";
+    #ballCounter = 0;
 
-    document.body.appendChild(boxMessage);
+    constructor(battingTeam, bowlingTeam) {
+        if ((battingTeam == undefined) || (bowlingTeam == undefined)) return;
 
-    /////////////////////////////////////////////////////////////////////////////
+        // set up batting team
+        if (battingTeam.teamname != undefined) { this.#teamBatName = battingTeam.teamname; }
+        // load batting team players
+        if (battingTeam.players.length > 0) {
+            this.#teamBat = new Array();
+            for (let index = 0; index < battingTeam.players.length; index++) {
+                const playerId = battingTeam.players[index];
+                this.#teamBat.push(new OwzatPlayerBattingRecord(BfxGetPlayerData(playerId)));
+            }
+        }
+        // load batting order
+        this.#teamBatOrder = battingTeam.battingOrder;
 
-    inningsRecord[0].initialise(teams[0]);
-    inningsRecord[1].initialise(teams[1]);
-    console.log(inningsRecord);
-
-    bowlingPair[0] = inningsRecord[idBowlingTeam].team.bowlingStarters[0];
-    bowlingPair[1] = inningsRecord[idBowlingTeam].team.bowlingStarters[1];
-
-    battingPair[0] = inningsRecord[idBattingTeam].team.battingOrder[0];
-    battingPair[1] = inningsRecord[idBattingTeam].team.battingOrder[1];
-
-    BfxStartMatch();
-}
-
-function BfxCreateButton(params) {
-    let btn = document.createElement("button");
-
-    btn.type = "button";
-    btn.textContent = params.caption;
-    btn.style.font = "inherit";
-    btn.style.margin = "2px";
-
-    btn.addEventListener("click", (ev) => {
-        params.func();
-    })
-
-    return btn;
-}
-
-function BfxCreateMessageBox() {
-    let box = document.createElement("div");
-
-    box.style.margin = "5px 0";
-    box.style.overflow = "auto";
-    box.style.borderTop = "solid 1px black";
-    box.style.borderBottom = "solid 1px black";
-
-    return box;
-}
-
-function BfxFormatBallCounter() {
-    let ball = (ballCounter % 6) + 1;
-    let over = Math.trunc(ballCounter / 6);
-
-    return over.toString() + "." + ball.toString();
-}
-
-function BfxStartMatch() {
-    let msg = document.createElement("div");
-    msg.style.backgroundColor = "rgba(0,0,0, 0.1)";
-    msg.innerHTML = "<b>" + inningsRecord[idBattingTeam].team.teamname + "</b> won the toss and decided to bat.";
-
-    boxMessage.insertAdjacentElement("afterbegin", msg);
-}
-
-function BfxBowlBall() {
-    let msg = document.createElement("div");
-    msg.style.borderBottom = (ballCounter % 6) == 0 ? "solid 1px black" : "dotted 1px rgba(100,100,100,0.5)";
-    msg.innerHTML = BfxFormatBallCounter() + " : ";
-
-    let bowlRandom = BfxRandomInt(6);
-    switch (bowlRandom) {
-        case 5:
-            msg.innerHTML += "<b style='color: red'>Owzat!</b>";
-            break;
-
-        default:
-            msg.innerHTML += OwzatFormatStrBoxMsgRunsScored(inningsRecord[idBowlingTeam].players[idBowler].name, inningsRecord[idBattingTeam].players[idBatsman].name, bowlRandom);
-            if ((bowlRandom % 2) == 1) idBatsman = Math.abs(idBatsman - 1);
-            break;
+        // set up bowling team
+        if (bowlingTeam.teamname != undefined) { this.#teamBowlName = bowlingTeam.teamname; }
+        // load bowling team players
+        if (bowlingTeam.players.length > 0) {
+            this.#teamBowl = new Array();
+            for (let index = 0; index < bowlingTeam.players.length; index++) {
+                const playerId = bowlingTeam.players[index];
+                this.#teamBowl.push(new OwzatPlayerBowlingRecord(BfxGetPlayerData(playerId)));
+            }
+        }
+        // load bowling order
+        this.#teamBowlOrder = bowlingTeam.bowlingStarters;
     }
-    ballCounter++;
 
-    boxMessage.insertAdjacentElement("afterbegin", msg);
+    get teamNameAtBat() { return this.#teamBatName; }
+    get teamNameAtBowl() { return this.#teamBowlName; }
+    get playerNameAtBat() { return this.#teamBat[this.#teamBatOrder[this.#currentBattingPair[this.#currentBatter]]].name; }
+    get playerNameAtBowl() { return this.#teamBowl[this.#teamBowlOrder[this.#currentBowlingPair[this.#currentBowler]]].name; }
+    get overCount() { return Math.trunc(this.#ballCounter / 6) + 1; }
+    get overBall() { return (this.#ballCounter % 6) + 1; }
 
-    BfxCheckOver();
-}
+    ProcessBowl() {
+        let runsScored = 0;
+        let bowlerScore = this.#teamBowl[this.#teamBowlOrder[this.#currentBowlingPair[this.#currentBowler]]].bowl;
+        let batterScore = this.#teamBat[this.#teamBatOrder[this.#currentBattingPair[this.#currentBatter]]].bat;
 
-function BfxBowlOver() {
-    for (let i = (ballCounter % 6); i < 6; i++) {
-        BfxBowlBall();
+        if (BfxRandomInt(100) < bowlerScore) {
+            // succesful bowl...
+            if (BfxRandomInt(100) < batterScore) {
+                // successful bat...
+                // console.log(bowlerScore.toString() + " Successful bowl, " + batterScore.toString() + " Successful bat.");
+                let batResult = BfxRandomInt(5) - 2;
+                if (batResult < 0) batResult = 0; // makes batResult either 0, 1 or 2 with a higher chance of 0
+
+                console.log(batResult);
+                if (BfxRandomInt(bowlerScore) < BfxRandomInt(batterScore)) {
+                    if (batResult == 2) {
+                        batResult = (BfxRandomInt(2) == 1) ? 6 : 4;
+                    } else {
+                        batResult *= BfxRandomInt(4);
+                    }
+                }
+
+                runsScored = batResult;
+
+                if ((runsScored % 2) == 1) { this.#currentBatter = Math.abs(this.#currentBatter - 1); }
+            } else {
+                // unsuccessful bat...
+                // console.log(bowlerScore.toString() + " Successful bowl, " + batterScore.toString() + " Unsuccessful bat.");
+            }
+        } else {
+            // unsuccessful bowl...
+            // console.log(bowlerScore.toString() + " Unsuccessful bowl.");
+        }
+
+        // after processing the bowl
+        this.#ballCounter++;
+        if (this.overBall == 1) { // start of new over
+            this.#currentBowler = Math.abs(this.#currentBowler - 1);
+            this.#currentBatter = Math.abs(this.#currentBatter - 1);
+        }
+        return { runs: runsScored, wicket: false };
     }
 }
 
-function BfxCheckOver() {
-    if ((ballCounter % 6) == 0) {
-        idBowler = Math.abs(idBowler - 1);
-        idBatsman = Math.abs(idBatsman - 1);
+class OwzatPlayerRecord { //===================================================
+    #id = '';
+    #forename = '';
+    #surname = '';
+    #nation = '';
+    #bat = 0;
+    #bowl = 0;
+    #field = 0;
+
+    constructor(playerData = undefined) {
+        if (playerData != undefined) {
+            if (playerData.id != undefined) { this.#id = playerData.id; }
+            if (playerData.forename != undefined) { this.#forename = playerData.forename; }
+            if (playerData.surname != undefined) { this.#surname = playerData.surname; }
+            if (playerData.nation != undefined) { this.#nation = playerData.nation; }
+            if (playerData.bat != undefined) { this.#bat = playerData.bat; }
+            if (playerData.bowl != undefined) { this.#bowl = playerData.bowl; }
+            if (playerData.field != undefined) { this.#field = playerData.field; }
+        }
+    }
+
+    get id() { return this.#id; }
+    get name() { return this.#surname + " " + this.#forename[0] + "."; }
+    get surname() { return this.#surname; }
+    get forename() { return this.#forename; }
+    get nation() { return this.#nation; }
+    get bat() { return this.#bat; }
+    get bowl() { return this.#bowl; }
+    get field() { return this.#field; }
+}
+
+class OwzatPlayerBattingRecord extends OwzatPlayerRecord { //==================
+    #balls = 0;
+    #runs = 0;
+    #status = '';
+
+    constructor(playerData = undefined) {
+        super(playerData);
+    }
+
+    get balls() { return this.#balls; }
+    get runs() { return this.#runs; }
+    get status() { return this.#status; }
+
+    AddBall(runs, status = '') {
+        this.#balls++;
+        this.#runs += runs;
+        if (status != '') this.#status = status;
     }
 }
+
+class OwzatPlayerBowlingRecord extends OwzatPlayerRecord { //==================
+    #balls = 0;
+    #runs = 0;
+    #wickets = 0;
+    #extras = 0;
+
+    constructor(playerData = undefined) {
+        super(playerData);
+    }
+
+    get balls() { return this.#balls; }
+    get runs() { return this.#runs; }
+    get wickets() { return this.#wickets; }
+    get extras() { return this.#extras; }
+
+    AddBall(runs, wicketBall = false) {
+        this.#balls++;
+        this.#runs += runs;
+        if (wicketBall) this.#wickets++;
+    }
+
+    AddNoBall() { this.#extras++; }
+}
+
+const test1 = new OwzatInningsRecord(teams[0], teams[1]);
+console.log(test1);
