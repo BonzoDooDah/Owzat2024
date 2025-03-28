bbde9a2aec1409d212e86bd372ed77c483711fc8
Scott Bukowski <scottbukowski@hotmail.com>
2025-03-28 21:17:16 +0000

    Initial commit.

---
+++ src/owzat.js
@@ -0,0 +1,35 @@
+const dbTestTeams = [
+    { id: 'ENGASH1984', name: 'England (Ashes 1984)', batting: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], bowling: [7, 10, 9, 8] },
+    { id: 'AUSASH1984', name: 'Australia (Ashes 1984)', batting: [11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21], bowling: [7, 9, 8, 10] }
+];
+
+const dbTestPlayers = [
+    { forename: 'Graham', surname: 'Gooch', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Mike', surname: 'Atherton', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Graeme', surname: 'Hick', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Matthew', surname: 'Maynard', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Nasser', surname: 'Hussain', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Alec', surname: 'Stewart', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Mark', surname: 'Ramprakash', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Angus', surname: 'Fraser', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Steve', surname: 'Watkin', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Peter', surname: 'Such', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Devon', surname: 'Malcolm', nation: 'ENG', teams: 'ENG-ASH93' },
+    { forename: 'Mark', surname: 'Taylor', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Michael', surname: 'Slater', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'David', surname: 'Boon', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Mark', surname: 'Waugh', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Allan', surname: 'Border', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Steve', surname: 'Waugh', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Ian', surname: 'Healy', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Merv', surname: 'Hughes', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Paul', surname: 'Reiffel', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Shane', surname: 'Warne', nation: 'AUS', teams: 'AUS-ASH93' },
+    { forename: 'Tim', surname: 'May', nation: 'AUS', teams: 'AUS-ASH93' }
+];
+
+console.clear();
+
+function initGame() {
+    // init game
+}
